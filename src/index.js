d4081fab4a3ea29e08e68086984691df2abc12c6
DebraChinhengo <dchinhengo@gmail.com>
2025-09-12 16:06:47 +1000

    Added Search functionality using API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,30 @@
+function showTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+}
+
+//make api call to openweathermap
+
+function searchCity(city) {
+  let apiKey = "28534ee264320foa541da835at649b33";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemperature);
+}
+
+//display weather info on page
+
 function handleSearchFormSubmit(event) {
   event.preventDefault();
 
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchFormSubmit);
+
+searchCity("Bindura");
